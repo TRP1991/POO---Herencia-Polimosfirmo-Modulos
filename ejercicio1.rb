513b3d8c0e38c54484c49b375c8916078c7f69df
Tamara Rubilar <ta.rubilar@gmail.com>
2020-08-10 01:37:34 -0400

    Se incorporan instrucciones a los archivos ejercicio1 y ejercicio2

---
+++ ejercicio1.rb
@@ -0,0 +1,4 @@
+=begin
+Utilizando los principios de Herencia y Polimorfismo, refactoriza el siguiente código para eliminar la
+innecesaria cantidad de sentencias if en los métodos ask e introduce. .(3 puntos )
+=end
