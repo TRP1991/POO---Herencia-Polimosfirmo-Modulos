c2ab784748d7b14c9927fbc537276baf69cc8c65
Tamara Rubilar <ta.rubilar@gmail.com>
2020-08-10 21:48:08 -0400

    Se desarrolla ejercicio1, refactorización

--- ejercicio1.rb
+++ ejercicio1.rb
@@ -2,3 +2,48 @@
 Utilizando los principios de Herencia y Polimorfismo, refactoriza el siguiente código para eliminar la
 innecesaria cantidad de sentencias if en los métodos ask e introduce. .(3 puntos )
 =end
+
+
+class Person
+    def initialize(first, last, age)
+        @first_name = first
+        @last_name = last
+        @age = age
+       
+    end
+    def birthday
+        @age += 1
+    end
+end
+
+class Student < Person
+    def talk
+        puts "Aquí es la clase de programación con Ruby?"
+    end
+
+    def introduce
+        puts "Hola profesor. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+class Teacher < Person
+    def talk
+        puts "Bienvenidos a la clase de programación con Ruby!"
+    end
+
+    def introduce
+        puts "Hola alumnos. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+class Parent < Person
+    def talk
+        puts "Aquí es la reunión de apoderados?"
+    end
+
+    def introduce
+        puts "Hola. Soy uno de los apoderados. Mi nombre es #{@first_name} #{@last_name}."
+    end
+end
+
+# print p1 = Parent.new('Tamara','Rubilar', 32).talk
